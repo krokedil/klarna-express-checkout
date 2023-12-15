626600a57277e1480602af0d162610223dd0dd2e
MichaelBengtsson <michael@krokedil.se>
2023-12-15 09:17:28 +0100

    Update main class to support not using session id from the client token
    We do this by unhooking KP from the checkout flow entirely while we have a customer token set for KEC.

--- src/KlarnaExpressCheckout.php
+++ src/KlarnaExpressCheckout.php
@@ -52,10 +52,45 @@ class KlarnaExpressCheckout {
 	 * @param string $options_key The option key to get the KEC settings from.
 	 */
 	public function __construct( $options_key = 'woocommerce_klarna_payments_settings' ) {
-		$this->settings            = new Settings( $options_key );
+		$this->settings = new Settings( $options_key );
+
 		$this->client_token_parser = new ClientTokenParser( $this->settings() );
 		$this->assets              = new Assets( $this->settings() );
 		$this->ajax                = new AJAX( $this->client_token_parser() );
+
+		add_action( 'init', array( $this, 'maybe_unhook_kp_actions' ), 15 );
+		add_action( 'woocommerce_single_product_summary', array( $this, 'add_kec_button' ), 31 );
+		add_action( 'woocommerce_thankyou', array( __CLASS__, 'unset_token' ), 1 );
+	}
+
+	/**
+	 * Set the client token to the WooCommerce session for the current cart contents only.
+	 *
+	 * @param string $token The cart token.
+	 */
+	public static function set_client_token( $token ) {
+		// Ensure session is initialized.
+		if ( ! WC()->session ) {
+			return false;
+		}
+
+		// Set the client token in a session using the cart_hash as a key.
+		WC()->session->set( 'kec_client_token', $token );
+	}
+
+	/**
+	 * Get the client token from the WooCommerce session.
+	 *
+	 * @return string|bool
+	 */
+	public static function get_client_token() {
+		// Ensure session is initialized.
+		if ( ! WC()->session ) {
+			return false;
+		}
+
+		// Get the client token from the session using the cart_hash as a key.
+		return WC()->session->get( 'kec_client_token' );
 	}
 
 	/**
@@ -65,6 +100,87 @@ class KlarnaExpressCheckout {
 	 */
 	public static function get_payment_button_id() {
 		return self::$payment_button_id;
+	}
+
+	/**
+	 * Unset the client token from the WooCommerce session.
+	 *
+	 * @return void
+	 */
+	public static function unset_token() {
+		// Ensure session is initialized.
+		if ( ! WC()->session ) {
+			return;
+		}
+
+		WC()->session->__unset( 'kec_client_token' );
+	}
+
+
+	/**
+	 * Maybe unhook Klarna Payments actions.
+	 *
+	 * @return void
+	 */
+	public function maybe_unhook_kp_actions() {
+		$client_token = self::get_client_token();
+		if ( empty( $client_token ) ) {
+			return;
+		}
+
+		// Ensure Klarna Payments exists.
+		if ( ! function_exists( 'KP_WC' ) ) {
+			return;
+		}
+
+		// Get the instances of the Klarna Payments classes we need to unhook actions from.
+		$kp_session  = KP_WC()->session;
+		$kp_checkout = KP_WC()->checkout;
+		$kp_gateway  = WC()->payment_gateways()->get_available_payment_gateways()['klarna_payments'];
+
+		// Unhook session actions.
+		remove_action( 'woocommerce_after_calculate_totals', array( $kp_session, 'get_session' ), 999999 );
+
+		// Unhook checkout actions.
+		remove_filter( 'woocommerce_update_order_review_fragments', array( $kp_checkout, 'add_token_fragment' ) );
+		remove_action( 'woocommerce_review_order_before_submit', array( $kp_checkout, 'html_client_token' ) );
+		remove_action( 'woocommerce_pay_order_before_submit', array( $kp_checkout, 'html_client_token' ) );
+
+		// Unhook gateway actions.
+		remove_action( 'wc_get_template', array( $kp_gateway, 'override_kp_payment_option' ) );
+
+		// Ensure we don't enqueue the KP scripts for the checkout page.
+		add_action( 'wp_enqueue_scripts', array( $this, 'dequeue_kp_scripts' ), 15 );
+	}
+
+	/**
+	 * Add the Klarna Express Checkout button to the product page.
+	 *
+	 * @return void
+	 */
+	public function add_kec_button() {
+		// Only show the button if KEC is enabled.
+		if ( ! $this->settings()->is_enabled() ) {
+			return;
+		}
+
+		// Ensure we only do this once per page load.
+		if ( did_action( 'woocommerce_single_product_summary' ) > 1 ) {
+			return;
+		}
+
+		?>
+		<div id="kec-pay-button"></div>
+		<?php
+	}
+
+	/**
+	 * Dequeue the Klarna Payments scripts.
+	 *
+	 * @return void
+	 */
+	public function dequeue_kp_scripts() {
+		wp_dequeue_script( 'klarna_payments' );
 	}
 
 	/**
