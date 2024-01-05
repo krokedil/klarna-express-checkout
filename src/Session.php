216a74c1f5c0bf29dece5faeb4b306462d007073
MichaelBengtsson <michael@krokedil.se>
2024-01-05 17:17:17 +0100

    Change all session class methods to be static

--- src/Session.php
+++ src/Session.php
@@ -15,9 +15,9 @@ class Session {
 	 * @return void
 	 */
 	public function __construct() {
-		add_action( 'woocommerce_checkout_update_order_review', array( $this, 'on_update_order_review' ) );
-		add_action( 'woocommerce_thankyou', array( __CLASS__, 'unset_client_token' ), 1 );
-		add_action( 'woocommerce_thankyou', array( __CLASS__, 'unset_klarna_address' ), 1 );
+		add_action( 'woocommerce_checkout_update_order_review', __CLASS__ . '::on_update_order_review' );
+		add_action( 'woocommerce_thankyou', __CLASS__ . '::unset_client_token', 1 );
+		add_action( 'woocommerce_thankyou', __CLASS__ . '::unset_klarna_address', 1 );
 	}
 
 	/**
@@ -27,7 +27,7 @@ class Session {
 	 *
 	 * @return void
 	 */
-	public function on_update_order_review( $posted_data ) {
+	public static function on_update_order_review( $posted_data ) {
 		// Try to get the client token and klarna address from the session.
 		$client_token   = self::get_client_token();
 		$klarna_address = self::get_klarna_address();
@@ -64,7 +64,7 @@ class Session {
 		);
 
 		foreach ( $address_fields_map as $klarna_field => $wc_field ) {
-			if ( ! $this->verify_address_field( $wc_field, $klarna_field, $parsed_data, $klarna_address, $ship_to_different_address ) ) {
+			if ( ! self::verify_address_field( $wc_field, $klarna_field, $parsed_data, $klarna_address, $ship_to_different_address ) ) {
 				// Clear the token and klarna address from the session.
 				self::unset_client_token();
 				self::unset_klarna_address();
@@ -86,7 +86,7 @@ class Session {
 	 *
 	 * @return bool
 	 */
-	private function verify_address_field( $wc_field, $klarna_field, $posted_data, $klarna_address, $ship_to_different_address ) {
+	private static function verify_address_field( $wc_field, $klarna_field, $posted_data, $klarna_address, $ship_to_different_address ) {
 		$billing_value  = isset( $posted_data[ 'billing_' . $wc_field ] ) ? $posted_data[ 'billing_' . $wc_field ] : '';
 		$shipping_value = isset( $posted_data[ 'shipping_' . $wc_field ] ) ? $posted_data[ 'shipping_' . $wc_field ] : '';
 		$klarna_value   = isset( $klarna_address[ $klarna_field ] ) ? $klarna_address[ $klarna_field ] : '';
@@ -103,14 +103,14 @@ class Session {
 			}
 
 			// Ensure we set the value to the customer.
-			$this->set_field( 'shipping_' . $wc_field, $shipping_value );
+			self::set_field( 'shipping_' . $wc_field, $shipping_value );
 			return false;
 		}
 
 		// Validate the billing field if the ship to different address checkbox is checked.
 		if ( ! $ship_to_different_address && $billing_value !== $klarna_value ) {
 			// Ensure we set the value to the customer.
-			$this->set_field( 'billing_' . $wc_field, $billing_value );
+			self::set_field( 'billing_' . $wc_field, $billing_value );
 			return false;
 		}
 
@@ -126,7 +126,7 @@ class Session {
 	 *
 	 * @return void
 	 */
-	private function set_field( $field, $value ) {
+	private static function set_field( $field, $value ) {
 		WC()->customer->set_props(
 			array(
 				$field => $value,
@@ -138,6 +138,8 @@ class Session {
 	 * Set the client token to the WooCommerce session.
 	 *
 	 * @param string $token The cart token.
+	 *
+	 * @return bool
 	 */
 	public static function set_client_token( $token ) {
 		// Ensure session is initialized.
@@ -147,12 +149,15 @@ class Session {
 
 		// Set the client token in a session using the cart_hash as a key.
 		WC()->session->set( 'kec_client_token', $token );
+		return true;
 	}
 
 	/**
 	 * Set the Klarna address to the WooCommerce sessions.
 	 *
 	 * @param array $address The Klarna address.
+	 *
+	 * @return bool
 	 */
 	public static function set_klarna_address( $address ) {
 		// Ensure session is initialized.
@@ -162,6 +167,8 @@ class Session {
 
 		// Set the Klarna address in a session using the cart_hash as a key.
 		WC()->session->set( 'kec_klarna_address', wp_json_encode( $address ) );
+
+		return true;
 	}
 
 	/**
@@ -203,28 +210,30 @@ class Session {
 	/**
 	 * Unset the client token from the WooCommerce session.
 	 *
-	 * @return void
+	 * @return bool
 	 */
 	public static function unset_client_token() {
 		// Ensure session is initialized.
 		if ( ! WC()->session ) {
-			return;
+			return false;
 		}
 
 		WC()->session->__unset( 'kec_client_token' );
+		return true;
 	}
 
 	/**
 	 * Unset the Klarna address from the WooCommerce session.
 	 *
-	 * @return void
+	 * @return bool
 	 */
 	public static function unset_klarna_address() {
 		// Ensure session is initialized.
 		if ( ! WC()->session ) {
-			return;
+			return false;
 		}
 
 		WC()->session->__unset( 'kec_klarna_address' );
+		return true;
 	}
 }
