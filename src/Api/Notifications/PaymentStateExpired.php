abbb2212d913581f81ab222a48c52d1d0c1517d0
MichaelBengtsson <michael@krokedil.se>
2025-11-14 14:05:28 +0100

    Update the PaymentStateExpired handler to cancel orders that expire

--- src/Api/Notifications/PaymentStateExpired.php
+++ src/Api/Notifications/PaymentStateExpired.php
@@ -21,6 +21,21 @@ class PaymentStateExpired extends Handler {
 	/**
 	 * @inheritDoc
 	 */
-	public function handle_notification($payload, $order = null) {
+	public function handle_notification($payload) {
+		$payment_request_id        = $payload['payment_request_id'] ?? null;
+		$interoperability_token    = $payload['interoperability_token'] ?? null;
+
+		if ( ! $payment_request_id || ! $interoperability_token ) {
+			throw new \WP_Exception( 'Missing required fields in the payload.' );
+		}
+
+		// Get the order by payment request ID.
+		$order = $this->get_wc_order_by_payment_request_id( $payment_request_id );
+
+		$order->update_status( 'cancelled', __( 'Order cancelled due to expired payment request.', 'krokedil-klarna-express-checkout' ) );
+
+		do_action( 'kec_cancel_order', $order, $interoperability_token, [], $payload['state'], $payload );
+
+		return null;
 	}
 }
