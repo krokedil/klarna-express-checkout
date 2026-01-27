b55269bddcc6fcc5ea8a766bb1052e1e77cad74c
anyakro <anya@krokedil.se>
2026-01-27 08:30:37 +0100

    Restructure JS & Remove sample code

--- assets/js/kec-admin.js
+++ assets/js/kec-admin.js
@@ -1,13 +1,28 @@
-jQuery(document).ready(function($) {
-    alert('KEC admin script loaded.');
+jQuery(function ($) {
+    const kec_admin = {
+        /**
+         * Initialize the KEC admin logic.
+         */
+        init() {
+            $('#woocommerce_klarna_payments_kec_flow').on('change', kec_admin.onFlowChange);
+            kec_admin.onFlowChange.call($('#woocommerce_klarna_payments_kec_flow'));
+        },
 
-    $('#woocommerce_klarna_payments_kec_flow').on('change', function() {
+        /**
+         * Handle the change event for the KEC flow setting.
+         * @returns {void}
+         */
+        onFlowChange() {
+            const value = $(this).val();
+            if (value === 'one_step') {
+                $('.kec-webhook-section').css('display', 'flex').show();
+                return;
+            }
+            $('.kec-webhook-section').hide();
+        },
+    };
 
-        if ( 'one_step' === $(this).val() ) {
-            $('.kec-webhook-section').css('display', 'flex').show();
-            return;
-        }
-
-        $('.kec-webhook-section').hide();
-    });
+    kec_admin.init();
 });
+
+
