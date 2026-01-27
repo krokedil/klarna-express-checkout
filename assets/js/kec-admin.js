9cc64d56c1324d73aa43515fdb560bfcbecc6100
anyakro <anya@krokedil.se>
2026-01-27 08:20:46 +0100

    Add admin script & CSS

---
+++ assets/js/kec-admin.js
@@ -0,0 +1,13 @@
+jQuery(document).ready(function($) {
+    alert('KEC admin script loaded.');
+
+    $('#woocommerce_klarna_payments_kec_flow').on('change', function() {
+
+        if ( 'one_step' === $(this).val() ) {
+            $('.kec-webhook-section').show();
+            return;
+        }
+
+        $('.kec-webhook-section').hide();
+    });
+});
