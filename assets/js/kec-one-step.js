15b753d0bd3b6f7837c3a09a83e087d871065c0b
anyakro <anya@krokedil.se>
2026-05-15 08:42:52 +0200

    Revert to interoperability_token const

--- assets/js/kec-one-step.js
+++ assets/js/kec-one-step.js
@@ -1,5 +1,5 @@
 const $ = jQuery;
-const { network_session } = await import("@klarna/interoperability_token");
+const { klarna_interoperability } = await import("@klarna/interoperability_token");
 let configData = {};
 
 const params = document.getElementById(
@@ -70,7 +70,7 @@ const KECOneStep = {
    */
   init: async function (e) {
     KECOneStep.params = configData;
-    KECOneStep.Klarna = network_session.Klarna;
+    KECOneStep.Klarna = klarna_interoperability.Klarna;
 
     KECOneStep.mountButton();
 
