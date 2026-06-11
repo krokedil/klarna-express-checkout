36982fee6a33e3e152da9a7d4ab31214a88503d9
anyakro <anya@krokedil.se>
2026-06-11 10:58:16 +0200

    Revert "Naming-only updates"
    This reverts commit aa529f9a2f74265b233ae491f4c1369ab5b9af4a.

--- src/Api/Notifications/PaymentStateExpired.php
+++ src/Api/Notifications/PaymentStateExpired.php
@@ -22,8 +22,8 @@ class PaymentStateExpired extends Handler {
 	 * @inheritDoc
 	 */
 	public function handle_notification( $payload ) {
-		$payment_request_id    = $payload['payment_request_id'] ?? null;
-		$network_session_token = $payload['klarna_network_session_token'] ?? '';
+		$payment_request_id     = $payload['payment_request_id'] ?? null;
+		$interoperability_token = $payload['klarna_network_session_token'] ?? '';
 
 		if ( ! $payment_request_id ) {
 			throw new \WP_Exception( 'Missing required fields in the payload.' );
@@ -34,7 +34,7 @@ class PaymentStateExpired extends Handler {
 
 		$order->update_status( 'cancelled', __( 'Order cancelled due to expired payment request.', 'krokedil-klarna-express-checkout' ) );
 
-		do_action( 'kec_cancel_order', $order, $network_session_token, array(), $payload['state'], $payload );
+		do_action( 'kec_cancel_order', $order, $interoperability_token, array(), $payload['state'], $payload );
 
 		return null;
 	}
